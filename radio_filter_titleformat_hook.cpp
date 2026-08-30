c8d706698a7fe7dca27f2e609526e1b46747887e
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2026-08-30 05:07:12 +0200

    Radio dynamic track descriptor %rdtd% titleformat field.

--- radio_filter_titleformat_hook.cpp
+++ radio_filter_titleformat_hook.cpp
@@ -2,16 +2,23 @@
 #include "utils.h"
 #include "radio_filter_titleformat_hook.h"
 
-void radio_filter_titleformat_hook::setData(std::vector<pfc::string8> vfilters) {
+void radio_filter_titleformat_hook::setData(pfc::string8 fdn, std::vector<pfc::string8> vfilters) {
 	m_vfilters = vfilters;
+	m_fdn = fdn;
 }
 
 bool radio_filter_titleformat_hook::process_field(titleformat_text_out *p_out,
 		const char *p_name,
 		t_size p_name_length,
 		bool &p_found_flag) {
-		p_found_flag = false;
-		return false;
+
+	if (!stricmp_utf8_ex(p_name, p_name_length, "rdtd", pfc_infinite)) {
+		p_out->write(titleformat_inputtypes::unknown, m_fdn.c_str(), m_fdn.get_length());
+		p_found_flag = true;
+		return true;
+	}
+	p_found_flag = false;
+	return false;
 }
 
 bool radio_filter_titleformat_hook::process_function(titleformat_text_out *p_out,
@@ -55,20 +62,8 @@ bool radio_filter_titleformat_hook::process_function(titleformat_text_out *p_out
 			t_size name_length;
 			p_params->get_param(0, name, name_length);
 
-			//todo: rev. checkfilter() in Prefs and upgradeDummy
-			filters::radio_nfo_type rnt;
-			filters::get_radio_nfo(name, rnt);
-			size_t primary_ok = rnt.primary_sig.first;
-			size_t radio_lensig = rnt.primary_sig.second;
-
-			pfc::string8 sigstr = name;
-			if (filters::check_radio_signature(radio_lensig != SIZE_MAX) && primary_ok != SIZE_MAX && primary_ok >= 2) {
-				sigstr = sigstr.subString(0, rnt.primary_sig.second);
-			}
-			//
-
-			auto it = std::find_if(m_vfilters.begin(), m_vfilters.end(), [sigstr](const pfc::string8 s) {
-				return strstr(sigstr, s.c_str()) != nullptr; });
+			auto it = std::find_if(m_vfilters.begin(), m_vfilters.end(), [name](const pfc::string8 s) {
+				return strstr(name, s.c_str()) != nullptr; });
 
 			if (strlen(name) == 0 || it == m_vfilters.end()) {
 				p_found_flag = true;
